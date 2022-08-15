77569f9e926c1e85f107729719e63651095bd7f0
msyesyan <msyesyan@gmail.com>
2022-08-15 11:02:28 +0800

    error repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, RequiredValidator, Validators } from '@angular/forms';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   template: `
-  <form nz-form [formGroup]="formGroup">
+  <p>大概是不支持 nz-container, nz-container ng-template 出去的组件，无法获取到 nzFormStatusService, 而动态 render template, 应该是一个稍微复杂一点的生产级前端所需要的正常需求，请问是否可以解决一下？</p>
+  <form nz-form [formGroup]="formGroup" nzLayout="vertical">
     <nz-form-item>
+      <nz-form-label>输入并删除，不会有错误状态</nz-form-label>
       <nz-form-control>
         <ng-container *ngTemplateOutlet="dynamicTpl"></ng-container>
       </nz-form-control>
     </nz-form-item>
+    <nz-form-item>
+      <nz-form-label>
+        正常交互
+      </nz-form-label>
+      <nz-form-control>
+      <input nz-input formControlName="name" />
+      </nz-form-control>
+    </nz-form-item>
     <ng-template #dynamicTpl>
-      <input formControlName="name" />
+      <input nz-input formControlName="name" />
     </ng-template>
   </form>
-  `
+  `,
 })
 export class AppComponent {
   formGroup = new FormGroup({
-    name: new FormControl(null, [Validators.required])
-  })
+    name: new FormControl(null, [Validators.required]),
+  });
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,7 +11,6 @@ import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
 import { IconDefinition } from '@ant-design/icons-angular';
 import * as AllIcons from '@ant-design/icons-angular/icons';
 
-
 import { AppComponent } from './app.component';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
@@ -22,21 +20,26 @@ registerLocaleData(en);
 const antDesignIcons = AllIcons as {
   [key: string]: IconDefinition;
 };
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(
+  (key) => antDesignIcons[key]
+);
 
 @NgModule({
-  imports:      [
-                  BrowserModule,
-                  CommonModule,
-                  FormsModule,
-                  HttpClientModule,
-                  ReactiveFormsModule,
-                  BrowserAnimationsModule,
-                  NzFormModule,
-                  NzInputModule,
-                ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ],
-  providers   : [ { provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons } ]
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NzFormModule,
+    NzInputModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    { provide: NZ_ICONS, useValue: icons },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
